e95e41464fd038ee1f4a04d0d17117efb5a454e7
FrancielyAS <franciely.unirios@gmail.com>
2025-05-13 10:11:45 -0300

    Código modificado

--- tela.py
+++ tela.py
@@ -1,1 +1,11 @@
-print("Curso Básico de Git e GitHub")
+import tkinter as tk
+
+janela = tk.Tk()
+
+janela.title("Python")
+
+janela.geometry("300x200")
+mensagem = tk.Label(janela, text="Olá! Bem-vindo ao Curso de Git e Github da UNIFAVIP WYDEN 😊")
+
+mensagem.pack()
+janela.mainloop()
